57eccb20689ff385dd6bb09388b57a82d0ec81c8
verakristina <verakristina784@gmail.com>
2022-06-25 09:47:37 +0700

    commit

---
+++ application/views/index.php
@@ -0,0 +1,217 @@
+     
+        <!--================Home Banner Area =================-->
+        <section class="home_banner_area">
+            <div class="banner_inner d-flex align-items-center">
+            	<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+				<div class="container">
+					<div class="banner_content text-center">
+                    <h3 data-aos="fade-up" data-aos-duration="1600">Belajar Dimana Saja & Kapan Saja <br />dengan Learnify Education</h3>
+                <p data-aos="fade-up" data-aos-duration="1900">Dengan Learnify Education kemudahan kegiatan belajar mengajar dapat terpenuhi. Para guru dan siswa dapat belajar meski banyak halangan atau rintangan. Pembelajaran Terstruktur dan Efektif hanya di Learnify Education! Nikmati kemudahan belajar dan materi terlengkap disini!</p>
+                <a data-aos="fade-up" data-aos-duration="2000" class="main_btn" href="<?php echo base_url()?>user">Daftar Sekarang <span class="lnr lnr-arrow-right"></span></a>
+					</div>
+				</div>
+            </div>
+        </section>
+        <!--================End Home Banner Area =================-->
+        
+        <!--================Finance Area =================-->
+        <section class="finance_area">
+        	<div class="container">
+        		<div class="finance_inner row">
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="finance_item">
+        					<div class="media">
+        						<div class="d-flex">
+        							<i class="lnr lnr-rocket"></i>
+        						</div>
+        						<div class="media-body">
+        							<h5>Science & <br />Engineering</h5>
+        						</div>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="finance_item">
+        					<div class="media">
+        						<div class="d-flex">
+        							<i class="lnr lnr-earth"></i>
+        						</div>
+        						<div class="media-body">
+        							<h5>Science & <br />Engineering</h5>
+        						</div>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="finance_item">
+        					<div class="media">
+        						<div class="d-flex">
+        							<i class="lnr lnr-smile"></i>
+        						</div>
+        						<div class="media-body">
+        							<h5>Science & <br />Engineering</h5>
+        						</div>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="finance_item">
+        					<div class="media">
+        						<div class="d-flex">
+        							<i class="lnr lnr-tag"></i>
+        						</div>
+        						<div class="media-body">
+        							<h5>Science & <br />Engineering</h5>
+        						</div>
+        					</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Finance Area =================-->
+        
+        <!--================Courses Area =================-->
+        <section class="courses_area p_120">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Popular Free Courses</h2>
+        			<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station.</p>
+        		</div>
+        		<div class="row courses_inner">
+        			<div class="col-lg-9">
+						<div class="grid_inner">
+							<div class="grid_item wd55">
+								<div class="courses_item">
+									<img src="assets/img/courses/course-1.jpg" alt="">
+									<div class="hover_text">
+										<a class="cat" href="#">Free</a>
+										<a href="#"><h4>Japanease Language Class</h4></a>
+										<ul class="list">
+											<li><a href="#"><i class="lnr lnr-users"></i> 355</a></li>
+											<li><a href="#"><i class="lnr lnr-bubble"></i> 35</a></li>
+											<li><a href="#"><i class="lnr lnr-user"></i> T. Robert</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="grid_item wd44">
+								<div class="courses_item">
+									<img src="assets/img/courses/course-2.jpg" alt="">
+									<div class="hover_text">
+										<a class="cat" href="#">Free</a>
+										<a href="#"><h4>Japanease Language Class</h4></a>
+										<ul class="list">
+											<li><a href="#"><i class="lnr lnr-users"></i> 355</a></li>
+											<li><a href="#"><i class="lnr lnr-bubble"></i> 35</a></li>
+											<li><a href="#"><i class="lnr lnr-user"></i> T. Robert</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="grid_item wd44">
+								<div class="courses_item">
+									<img src="assets/img/courses/course-4.jpg" alt="">
+									<div class="hover_text">
+										<a class="cat" href="#">Free</a>
+										<a href="#"><h4>Japanease Language Class</h4></a>
+										<ul class="list">
+											<li><a href="#"><i class="lnr lnr-users"></i> 355</a></li>
+											<li><a href="#"><i class="lnr lnr-bubble"></i> 35</a></li>
+											<li><a href="#"><i class="lnr lnr-user"></i> T. Robert</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="grid_item wd55">
+								<div class="courses_item">
+									<img src="assets/img/courses/course-5.jpg" alt="">
+									<div class="hover_text">
+										<a class="cat" href="#">Free</a>
+										<a href="#"><h4>Japanease Language Class</h4></a>
+										<ul class="list">
+											<li><a href="#"><i class="lnr lnr-users"></i> 355</a></li>
+											<li><a href="#"><i class="lnr lnr-bubble"></i> 35</a></li>
+											<li><a href="#"><i class="lnr lnr-user"></i> T. Robert</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+        			</div>
+        			<div class="col-lg-3">
+        				<div class="course_item">
+							<img src="assets/img/courses/course-3.jpg" alt="">
+							<div class="hover_text">
+								<a class="cat" href="#">Free</a>
+								<a href="#"><h4>Japanease Language Class</h4></a>
+								<ul class="list">
+									<li><a href="#"><i class="lnr lnr-users"></i> 355</a></li>
+									<li><a href="#"><i class="lnr lnr-bubble"></i> 35</a></li>
+									<li><a href="#"><i class="lnr lnr-user"></i> T. Robert</a></li>
+								</ul>
+							</div>
+						</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Courses Area =================-->
+    
+        <!--================Latest Blog Area =================-->
+        <section class="latest_blog_area p_120">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Latest Posts From Blog</h2>
+        			<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station.</p>
+        		</div>
+        		<div class="row latest_blog_inner">
+        			<div class="col-lg-3 col-md-6">
+        				<div class="l_blog_item">
+        					<img class="img-fluid" src="assets/img/latest-blog/l-blog-1.jpg" alt="">
+        					<a class="date" href="#">25 October, 2018  |  By Mark Wiens</a>
+        					<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>
+        					<p>Computers have become ubiquitous in almost every facet of our lives. At work, desk jockeys spend hours in front of their desktops, while delivery</p>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-md-6">
+        				<div class="l_blog_item">
+        					<img class="img-fluid" src="assets/img/latest-blog/l-blog-2.jpg" alt="">
+        					<a class="date" href="#">25 October, 2018  |  By Mark Wiens</a>
+        					<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>
+        					<p>Computers have become ubiquitous in almost every facet of our lives. At work, desk jockeys spend hours in front of their desktops, while delivery</p>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-md-6">
+        				<div class="l_blog_item">
+        					<img class="img-fluid" src="assets/img/latest-blog/l-blog-3.jpg" alt="">
+        					<a class="date" href="#">25 October, 2018  |  By Mark Wiens</a>
+        					<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>
+        					<p>Computers have become ubiquitous in almost every facet of our lives. At work, desk jockeys spend hours in front of their desktops, while delivery</p>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-md-6">
+        				<div class="l_blog_item">
+        					<img class="img-fluid" src="assets/img/latest-blog/l-blog-4.jpg" alt="">
+        					<a class="date" href="#">25 October, 2018  |  By Mark Wiens</a>
+        					<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>
+        					<p>Computers have become ubiquitous in almost every facet of our lives. At work, desk jockeys spend hours in front of their desktops, while delivery</p>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Latest Blog Area =================-->
+        
+        <!--================Impress Area =================-->
+        <section class="impress_area p_120">
+        	<div class="container">
+        		<div class="impress_inner text-center">
+					<h2>Become an instructor</h2>
+					<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station whether that is on the deck</p>
+					<a class="main_btn2" href="#">Apply for the post</a>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Impress Area =================-->
+        
---
+++ application/views/template/header.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="assets/img/favicon.png" type="image/png">
+        <title>Learn IT Education</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/vendors/linericon/style.css">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/vendors/lightbox/simpleLightbox.css">
+        <link rel="stylesheet" href="assets/vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="assets/vendors/animate-css/animate.css">
+        <link rel="stylesheet" href="assets/vendors/popup/magnific-popup.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+    </head>
+    <body>
+        
+        <!--================Header Menu Area =================-->
+        <header class="header_area">
+           	<div class="top_menu row m0">
+           		<div class="container">
+					<div class="float-left">
+						<ul class="list header_social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+							<li><a href="#"><i class="fa fa-behance"></i></a></li>
+						</ul>
+					</div>
+					<div class="float-right">
+						<a class="dn_btn" href="tel:+62 8810 2481 7597">+62 8810 2481 7597</a>
+						<a class="dn_btn" href="mailto:elearning@gmail.com">elearning@gmail.com</a>
+					</div>
+           		</div>	
+           	</div>	
+            <div class="main_menu">
+            	<nav class="navbar navbar-expand-lg navbar-light">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<a class="navbar-brand logo_h" href="index.html"><img src="assets/img/logo.png" alt=""></a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+							<ul class="nav navbar-nav menu_nav ml-auto">
+								<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li> 
+								<li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li> 
+								<li class="nav-item submenu dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Courses</a>
+									<ul class="dropdown-menu">
+										<li class="nav-item"><a class="nav-link" href="courses.html">Courses</a>
+										<li class="nav-item"><a class="nav-link" href="course-details.html">Course Details</a></li>
+									</ul>
+								</li> 
+								<li class="nav-item"><a class="nav-link" href="contact.html">Login</a></li>
+							</ul>
+						</div> 
+					</div>
+            	</nav>
+            </div>
+        </header>
+        <!--================Header Menu Area =================-->
